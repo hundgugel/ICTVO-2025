f9aafaa991299e0c6e8b8f66d61de70289742010
hundgugel <yanuk144@gmail.com>
2025-06-19 21:46:28 +0300

    Add files via upload

--- hw5/planet.js
+++ hw5/planet.js
@@ -28,31 +28,34 @@ AFRAME.registerComponent('planet', {
  	}
 });
 
-
 AFRAME.registerComponent('main', {
- 	init: function() {
- 		this.solar_system = document.querySelectorAll('[planet]');
+ init: function() {
+ this.solar_system = document.querySelectorAll('[planet]');
  },
  tick: function (time, deltaTime) {
- 		for(var i = 0; i<this.solar_system.length; i++) {
- 		planet_i=this.solar_system[i].getAttribute('planet');
-		planet_i.a[0]=planet_i.a[1]=planet_i.a[2]=0;
- 		for(var j = 0; j<this.solar_system.length; j++) {
- 			planet_j=this.solar_system[j].getAttribute('planet');
- 			if(i!=j) {
- 				deltapos = [0,0,0];
- 				for(var k = 0; k < 3; k++)
- 					deltapos[k]=planet_j.pos[k]-planet_i.pos[k];
- 				var r=Math.sqrt(Math.pow(deltapos[0],2)+Math.pow(deltapos[1],2)+Math.pow(deltapos[2],2));
- 				for(var k = 0; k < 3; k++)
- 					planet_i.a[k]+=G*planet_j.mass*deltapos[k]/Math.pow(r, 3);
- 			}
- 		}
- 		for(var k = 0; k < 3; k++)
- 			planet_i.v[k]+=planet_i.a[k]*dt;
- 		for(var k = 0; k < 3; k++)
- 			planet_i.pos[k]+=planet_i.v[k]*dt;
- 		this.solar_system[i].setAttribute('position', (planet_i.pos[0]/1e9)+' '+(planet_i.pos[1]/1e9)+' '+(planet_i.pos[2]/1e9));
- 		}
- 	}
+ for(var i = 0; i<this.solar_system.length; i++) {
+ planet_i=this.solar_system[i].getAttribute('planet');
+ planet_i.a[0]=planet_i.a[1]=planet_i.a[2]=0;
+ for(var j = 0; j<this.solar_system.length; j++) {
+ planet_j=this.solar_system[j].getAttribute('planet');
+ if(i!=j) {
+ deltapos = [0,0,0];
+ for(var k = 0; k < 3; k++)
+ deltapos[k]=planet_j.pos[k]-planet_i.pos[k];
+ var r=Math.sqrt(Math.pow(deltapos[0],2)+
+ Math.pow(deltapos[1],2)+Math.pow(deltapos[2],2));
+ for(var k = 0; k < 3; k++)
+ planet_i.a[k]+=G*planet_j.mass*deltapos[k]/
+ Math.pow(r, 3);
+ }
+ }
+ for(var k = 0; k < 3; k++)
+ planet_i.v[k]+=planet_i.a[k]*dt;
+ for(var k = 0; k < 3; k++)
+ planet_i.pos[k]+=planet_i.v[k]*dt;
+ this.solar_system[i].setAttribute('position',
+ (planet_i.pos[0]/1e9)+' '+(planet_i.pos[1]/1e9)+
+ ' '+(planet_i.pos[2]/1e9));
+ }
+ }
  });
